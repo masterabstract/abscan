0083617339dd6ee494f8d015019f60aad51b4c3c
masterabstract <masterabstractx@gmail.com>
2026-03-01 21:50:29 +0100

    fix landing page route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import Collection from './pages/Collection';
 import Submit from './pages/Submit';
@@ -7,10 +8,11 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/collection/:slug" element={<Collection />} />
         <Route path="/submit" element={<Submit />} />
-        <Route path="*" element={<Dashboard />} />
+        <Route path="*" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
