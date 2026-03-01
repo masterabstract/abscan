d9debb57ee1bc79e8983dab03299c6ec8de159d0
masterabstract <masterabstractx@gmail.com>
2026-03-01 18:51:01 +0100

    fix abstract chain

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AbstractWalletProvider } from '@abstract-foundation/agw-react';
+import { abstract } from 'viem/chains';
 import './index.css';
 import App from './App.jsx';
 
@@ -13,7 +14,7 @@ const queryClient = new QueryClient({
 
 createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={queryClient}>
-    <AbstractWalletProvider chain="abstract">
+    <AbstractWalletProvider chain={abstract}>
       <App />
     </AbstractWalletProvider>
   </QueryClientProvider>
