4b9b207b489c010fb9e4bbdb7304374da5d1e33d
masterabstract <masterabstractx@gmail.com>
2026-03-01 20:41:25 +0100

    suppression connexion wallet

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Connect from './pages/Connect';
 import Dashboard from './pages/Dashboard';
 import Collection from './pages/Collection';
 
@@ -8,11 +7,9 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/connect" element={<Connect />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Dashboard />} />
         <Route path="/collection/:slug" element={<Collection />} />
-        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="*" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
